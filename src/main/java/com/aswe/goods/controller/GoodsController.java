c95f867611ea361d26443add91fc493ccb38c622
jaebeom <jaebum7396@naver.com>
2023-09-04 00:53:39 +0900

    feat. GoodsController 추가

--- src/main/java/com/aswe/goods/controller/GoodsController.java
+++ src/main/java/com/aswe/goods/controller/GoodsController.java
@@ -2,11 +2,7 @@ package com.aswe.goods.controller;
 
 import com.aswe.common.CommonUtils;
 import com.aswe.goods.service.GoodsService;
-import com.aswe.user.model.LoginRequest;
-import com.aswe.user.model.Response;
 import com.aswe.user.model.SignupRequest;
-import com.aswe.user.service.AuthService;
-import com.aswe.user.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,11 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @Slf4j
 @Api(tags = "GoodsController")
